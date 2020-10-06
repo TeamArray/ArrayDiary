e7860b47cabc4d483fbddf30a23645ffeaf8da40
Park Min Hyeok <pmhstudio.pmh@gmail.com>
2020-10-06 10:46:02 +0900

    Diary Collector

--- events/onMessage.js
+++ events/onMessage.js
@@ -4,11 +4,22 @@ const Query = require('../classes/Query')
  * @param {import('../classes/Client')} client
  * @param {import('discord.js').Message} msg
  */
-function onMessage (client, msg) {
+async function onMessage (client, msg) {
   const { prefix } = client.settings
   const { author, content } = msg
 
   if (author.bot) return
+
+  if (msg.channel.parentID === client.settings.target) {
+    if (msg.content.length < client.settings.minchar) {
+      const m = await msg.channel.send('40자 미만의 메세지는 일기로 카운트 되지 않습니다')
+      m.delete({ timeout: 1000 })
+    } else {
+      await client.db.insert({ id: msg.id, author: msg.author.id, length: msg.content.length }).into('diaries')
+      msg.react(client.emojis.cache.get('762703472293314561'))
+    }
+  }
+
   if (!content.startsWith(prefix)) return
 
   const query = new Query(prefix, content)
--- events/onReady.js
+++ events/onReady.js
@@ -1,11 +1,22 @@
 /**
  * @param {import('../classes/Client')} client
  */
-function onReady (client) {
+async function onReady (client) {
   console.log(
     client.user.username + ' is now online!\n' +
     'prefix: ' + client.settings.prefix
   )
+
+  const { children } = client.channels.resolve(client.settings.target)
+  const channels = children.array().filter((chn) => chn.type === 'text')
+  for (const channel of channels) {
+    const channelmsgs = await channel.messages.fetch()
+    for (const [, msg] of channelmsgs) {
+      if (msg.content.length < client.settings.minchar) continue
+      const [exist] = await client.db.select('id').where('id', msg.id).from('diaries')
+      if (!exist) await client.db.insert({ id: msg.id, author: msg.author.id, length: msg.content.length, createdAt: msg.createdAt }).into('diaries')
+    }
+  }
 }
 
 module.exports = onReady
